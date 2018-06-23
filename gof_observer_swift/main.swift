6815d93dd2e73641a247301c41318301a376b0d3
Roman <cold.steel.br@gmail.com>
2018-06-23 11:05:45 +0700

    Init

---
+++ gof_observer_swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  gof_observer_swift
+//
+//  Created by Roman Brazhnikov on 23.06.2018.
+//  Copyright © 2018 Roman Brazhnikov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ gof_observer_swift/observer/Publisher.swift
@@ -0,0 +1,9 @@
+//
+//  Publisher.swift
+//  gof_observer_swift
+//
+//  Created by Roman Brazhnikov on 23.06.2018.
+//  Copyright © 2018 Roman Brazhnikov. All rights reserved.
+//
+
+import Foundation
